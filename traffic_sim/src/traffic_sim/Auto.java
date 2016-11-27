1cfe2ed548446137c5b557d53976383343a82763
ddand_000 <ddand_000@Daniel-NB>
2016-11-26 18:17:23 -0800

    first commit

--- traffic_sim/src/traffic_sim/Auto.java
+++ traffic_sim/src/traffic_sim/Auto.java
@@ -1,5 +1,13 @@
 package traffic_sim;
 
 public class Auto {
-
+	private int lane;
+	private int pos;
+	private int behaviour;
+	
+	public Auto(int lane, double pos, int behaviour) {
+		lane = this.lane;
+		pos = this.pos;
+		behaviour = this.behaviour;
+	}
 }
--- traffic_sim/src/traffic_sim/SimSpace.java
+++ traffic_sim/src/traffic_sim/SimSpace.java
@@ -1,5 +1,26 @@
 package traffic_sim;
 
 public class SimSpace {
-
+	private static Auto[] auto_arr;
+	
+	private static final int C_LEN = 5;
+	private static final double DENSITY = 0.5;
+	private static final double T_LEN = 100;
+	private static final double[] C_PROP = {0.25,0.25,0.25,0.25};
+	private static final double[] B_PROP = {0,1};
+	
+	public static void main(String[] args) {
+	
+		
+		
+	}
+	
+	public void genInit(int type) {
+		
+		
+	}
+	
+	public void Snapshot() {
+		
+	}
 }
