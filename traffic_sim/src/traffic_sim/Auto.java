935d12920823bcd2bc190a1224f472ec7bb8fc07
ddand_000 <ddand_000@Daniel-NB>
2016-11-26 23:11:41 -0800

    commit 2

--- traffic_sim/src/traffic_sim/Auto.java
+++ traffic_sim/src/traffic_sim/Auto.java
@@ -4,10 +4,25 @@ public class Auto {
 	private int lane;
 	private int pos;
 	private int behaviour;
+	private double current_speed;
+	private double desired_speed;
+	private double decel_rate;
+	private double acel_rate;
+	private double fwd_thresh;
+	private double bwd_thresh;
 	
-	public Auto(int lane, double pos, int behaviour) {
+	public Auto(int lane, double pos, int behaviour, double current_speed, double desired_speed, 
+			double decel_rate, double acel_rate, double fwd_thresh, double bwd_thresh) {
 		lane = this.lane;
 		pos = this.pos;
-		behaviour = this.behaviour;
+		behaviour = this.behaviour;		
+		current_speed = this.current_speed;
+		desired_speed = this.desired_speed;
+		decel_rate = this.decel_rate;
+		acel_rate = this.acel_rate;
+		fwd_thresh = this.fwd_thresh;
+		bwd_thresh = this.bwd_thresh;
 	}
+	
+	
 }
--- traffic_sim/src/traffic_sim/SimSpace.java
+++ traffic_sim/src/traffic_sim/SimSpace.java
@@ -4,23 +4,50 @@ public class SimSpace {
 	private static Auto[] auto_arr;
 	
 	private static final int C_LEN = 5;
-	private static final double DENSITY = 0.5;
-	private static final double T_LEN = 100;
+	private static final double TRACK_LEN = 1000.0;
+	private static final double OBS_LEN = 100.0;
+
+	private static final double density = 0.5;
+	private static final int type = 1;
+	
 	private static final double[] C_PROP = {0.25,0.25,0.25,0.25};
-	private static final double[] B_PROP = {0,1};
+	private static final double[] B_PROP = {.5,.5};
+	
+	private static final int STEPS = 100;
+	private static final double STEP_SIZE = .01;
+	
+	private static final double DECEL_RATE = 2.0;
+	private static final double ACEL_RATE = 2.0;
+	
+	private static final double AVG_SPEED = 50.0;
+	
+	private static final double FWD_SWITCH_THRESH = 10.0;
+	private static final double SPD_SWITCH_THRESH = 10.0;
+	
+	private static final double BWD_SWITCH_THRESH = 10.0;
+	private static final double SDE_SWITCH_THRESH = 10.0;
 	
 	public static void main(String[] args) {
-	
+		genInit(type);
 		
 		
 	}
-	
-	public void genInit(int type) {
-		
+	public static void runSim() {
 		
 	}
 	
-	public void Snapshot() {
+	public static void runSim(int steps) {
+		
+	}
+	
+	public static void genInit(int type) {
+		if (type == 1) {
+			
+		}
+		
+	}
+	
+	public static void Snapshot() {
 		
 	}
 }
