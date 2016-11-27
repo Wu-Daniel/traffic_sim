333e0d7dcf9eb06816e75bef7554191f51bc89ee
ddand_000 <ddand_000@Daniel-NB>
2016-11-27 00:46:23 -0800

    commit 3, Auto.java needs behaviour 2 in, setting up simulation functions. starting by generating init conditions.

--- traffic_sim/src/traffic_sim/Auto.java
+++ traffic_sim/src/traffic_sim/Auto.java
@@ -1,28 +1,92 @@
 package traffic_sim;
 
 public class Auto {
+	private int exist;
+	
 	private int lane;
 	private int pos;
 	private int behaviour;
+	
 	private double current_speed;
 	private double desired_speed;
+	
 	private double decel_rate;
 	private double acel_rate;
+	
+	private double slow_thresh;
+	
 	private double fwd_thresh;
-	private double bwd_thresh;
+	private double spd_thresh;
+	
+	private double sde_thresh;
+	
+	public Auto() {
+		exist = 0;
+	}
 	
 	public Auto(int lane, double pos, int behaviour, double current_speed, double desired_speed, 
-			double decel_rate, double acel_rate, double fwd_thresh, double bwd_thresh) {
+			double decel_rate, double acel_rate, double slow_thresh, double fwd_thresh, 
+			double spd_thresh, double sde_thresh) {
+		
 		lane = this.lane;
 		pos = this.pos;
 		behaviour = this.behaviour;		
+		
 		current_speed = this.current_speed;
 		desired_speed = this.desired_speed;
+		
 		decel_rate = this.decel_rate;
 		acel_rate = this.acel_rate;
+		
+		slow_thresh = this.slow_thresh;
+		
 		fwd_thresh = this.fwd_thresh;
-		bwd_thresh = this.bwd_thresh;
+		spd_thresh = this.spd_thresh;
+		
+		sde_thresh = this.sde_thresh;		
+		
+		exist = 1;
 	}
 	
+	public boolean exist() {
+		if (exist == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getLane() {
+		return lane;
+	}
 	
+	public int getPos() {
+		return pos;
+	}
+	
+	public double getSpeed() {
+		return current_speed;
+	}
+	
+	public void step(Auto auto_up_f, Auto auto_up_b, Auto auto_down_f, Auto auto_down_b, Auto auto_forward, 
+			double step_size) {
+		
+		if (behaviour == 1) {
+			if (current_speed < desired_speed) {
+				if (auto_forward.exist()) {
+					if (auto_forward.getPos() - pos < fwd_thresh) {
+						if (auto_forward.getSpeed() < current_speed) {
+							current_speed -= decel_rate * step_size;
+						}
+					} else {
+						current_speed += acel_rate * step_size;
+					}
+				} else {
+					current_speed += acel_rate * step_size;
+				}
+			}
+		} else  if (behaviour == 2) {
+			
+		}
+	}
 }
--- traffic_sim/src/traffic_sim/SimSpace.java
+++ traffic_sim/src/traffic_sim/SimSpace.java
@@ -1,35 +1,46 @@
 package traffic_sim;
 
+import java.util.*;
+
 public class SimSpace {
-	private static Auto[] auto_arr;
+	private static ArrayList<Auto> auto_arr = new ArrayList<Auto>();
+	private static double[] c_prop; 
 	
 	private static final int C_LEN = 5;
 	private static final double TRACK_LEN = 1000.0;
 	private static final double OBS_LEN = 100.0;
 
-	private static final double density = 0.5;
-	private static final int type = 1;
+	private static final double DENSITY = 0.5;
+	private static final int TYPE = 1;
 	
 	private static final double[] C_PROP = {0.25,0.25,0.25,0.25};
 	private static final double[] B_PROP = {.5,.5};
 	
 	private static final int STEPS = 100;
 	private static final double STEP_SIZE = .01;
+	private static final int STEP_REPROP = 10;
 	
-	private static final double DECEL_RATE = 2.0;
-	private static final double ACEL_RATE = 2.0;
+	private static final double DECEL_RATE = 5.0;
+	private static final double ACEL_RATE = 5.0;
+	private static final double ACCEL_VAR = 1.0;
 	
 	private static final double AVG_SPEED = 50.0;
+	private static final double SPEED_VAR = 5.0;
+	
+	private static final double SLOW_DIST_THRESH = 10.0;
+	private static final double SLOW_DIST_VAR = 5.0;
 	
 	private static final double FWD_SWITCH_THRESH = 10.0;
+	private static final double FWD_SWITCH_VAR = 5.0;
 	private static final double SPD_SWITCH_THRESH = 10.0;
+	private static final double SPD_SWITCH_VAR = 5.0;
 	
-	private static final double BWD_SWITCH_THRESH = 10.0;
 	private static final double SDE_SWITCH_THRESH = 10.0;
+	private static final double SDE_SWITCH_VAR = 5.0;
 	
 	public static void main(String[] args) {
-		genInit(type);
-		
+		genInit(TYPE);
+		System.out.println("gen finish ... ");
 		
 	}
 	public static void runSim() {
@@ -41,13 +52,30 @@ public class SimSpace {
 	}
 	
 	public static void genInit(int type) {
+		c_prop = C_PROP;
+		int num_lane = c_prop.length;
+		
+		// density = num_car * c_len / track_len
+		int cpl =  (int) (num_lane * ((DENSITY * TRACK_LEN / (C_LEN) ) - 1)); 
+		
 		if (type == 1) {
-			
+			for (int i = 0; i < num_lane; i++) {
+				for (int j = 0; j < cpl; j++ ) {
+					int behav;
+					if (Math.random() < .5) {
+						behav = 0;
+					} else {
+						behav = 1;
+					}
+					Auto temp = new Auto(i,(j * (i % 2)) * (TRACK_LEN / (cpl + 1)), 1 /*behav*/,);
+					auto_arr.add(temp);
+				}
+			}
 		}
 		
 	}
 	
-	public static void Snapshot() {
+	public static void snapshot() {
 		
 	}
 }
